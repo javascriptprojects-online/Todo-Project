2de43111a5ca885478271be01c9bb92e2b7ec2f6
javascriptprojects-online <ssridevirao@gmail.com>
2026-03-10 14:02:15 -0600

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,39 @@
+let todoList = [
+    {
+     item: 'buy milk',
+     dueDate: '02/09/2026'
+    },
+    {
+     item: 'Go to college',
+     dueDate: '02/09/2026'
+    }
+ ];
+displayItems();
+
+function addTodo(){
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
+    console.log(todoList);
+    inputElement.value = '';
+    dateElement.value = '';
+    displayItems();
+}
+
+function displayItems(){
+    let containerElement = document.querySelector('.todo-container');
+    let newHtml = '';
+    
+    for(let i=0;i<todoList.length; i++){
+       
+        let {item, dueDate} = todoList[i];
+        newHtml+=`
+               <span>${item}</span>
+               <span>${dueDate}</span>
+               <button class='btn-delete' onclick="todoList.splice(${i}, 1); displayItems();" >Delete</button>
+        `;
+    }
+    containerElement.innerHTML = newHtml;
+}
